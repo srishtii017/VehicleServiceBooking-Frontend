94d4e252d11f0e9d523e118586573b12f41a4293
Srishti Gupta <2492348@cognizant.com>
2026-06-15 15:57:00 +0530

    feat: setup of structure

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import {Login} from './features/user/login/login';
+import {Profile} from './features/user/profile/profile';
+import {Register} from './features/user/register/register';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+ { path: 'login', component: Login},
+ { path: 'register', component: Register},
+ {path: 'profile', component: Profile},
+
+ //Default redirect when app starts up
+ {path: '', redirectTo: 'login', pathMatch: 'full'}
+
+];
