2430d93bd1a81106d26fd1affc094b0dba0ff1d6
Srishti Gupta <2492348@cognizant.com>
2026-06-16 23:15:04 +0530

    wip:added components before sync

---
+++ src/app/features/user/change-password/change-password.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-change-password',
+  imports: [],
+  templateUrl: './change-password.html',
+  styleUrl: './change-password.css',
+})
+export class ChangePassword {}
