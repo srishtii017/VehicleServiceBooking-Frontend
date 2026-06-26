50c8782cddc281b7fe042955a72c322102d99e41
Srishti Gupta <2492348@cognizant.com>
2026-06-26 18:36:40 +0530

    feat:validations added

--- src/app/features/owner/login-owner/login-owner.ts
+++ src/app/features/owner/login-owner/login-owner.ts
@@ -5,11 +5,14 @@ import { OwnerLogin } from '../Models/login';
 import { OwnerLoginService } from '../Services/owner-login-service';
 import { OwnerAuthService } from '../Services/owner-auth.service';
 import { ToastrService } from 'ngx-toastr';
+import { NgForm } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-login-owner',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, CommonModule],
   templateUrl: './login-owner.html',
   styleUrl: './login-owner.css',
 })
@@ -26,33 +29,40 @@ export class LoginOwner {
     this.router.navigate(['/owner/register']);
   }
 
-  HandleLogin() {
-    this.isLoading = true;
+ HandleLogin(form: NgForm) {
 
-    this.loginService.LoginOwner(this.login).subscribe({
-      next: (res: any) => {
-        this.isLoading = false;
-        this.authService.loginOwner(res.data);
-        
-        this.toastr.success('Welcome back, boss!', 'Login Successful', {
-          timeOut: 2500,
-          progressBar: true,
-          closeButton: true
-        });
-
-        this.router.navigate(['/main']);
-        console.log(this.authService.isLoggedIn());
-      },
-      error: (error) => {
-        this.isLoading = false;
-        const msg = error.error?.message || 'Login failed.';
-        
-        this.toastr.error(msg, 'Auth Error', {
-          timeOut: 4000,
-          progressBar: true,
-          closeButton: true
-        });
-      }
-    });
+  if (form.invalid) {
+    form.control.markAllAsTouched(); // show errors
+    return;
   }
+
+  this.isLoading = true;
+
+  this.loginService.LoginOwner(this.login).subscribe({
+    next: (res: any) => {
+      this.isLoading = false;
+      this.authService.loginOwner(res.data);
+      
+      this.toastr.success('Welcome back, boss!', 'Login Successful', {
+        timeOut: 2500,
+        progressBar: true,
+        closeButton: true
+      });
+
+      this.router.navigate(['/main']);
+      console.log(this.authService.isLoggedIn());
+    },
+    error: (error) => {
+      this.isLoading = false;
+      const msg = error.error?.message || 'Login failed.';
+      
+      this.toastr.error(msg, 'Auth Error', {
+        timeOut: 4000,
+        progressBar: true,
+        closeButton: true
+      });
+    }
+  });
+}
+
 }
--- src/app/features/owner/register-owner/register-owner.ts
+++ src/app/features/owner/register-owner/register-owner.ts
@@ -4,11 +4,13 @@ import { FormsModule } from '@angular/forms';
 import { OwnerRegister } from '../Models/register';
 import { OwnerRegisterService } from '../Services/owner-register-service';
 import { ToastrService } from 'ngx-toastr';
+import { CommonModule } from '@angular/common';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-register-owner',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, CommonModule],
   templateUrl: './register-owner.html',
   styleUrl: './register-owner.css',
 })
@@ -25,32 +27,38 @@ export class RegisterOwner {
     this.router.navigate(['/owner/login']);
   }
 
-  HandleRegister() {
-    this.isLoading = true;
+  HandleRegister(form: NgForm) {
 
-    this.RegisterService.RegisterOwner(this.reg).subscribe({
-      next: () => {
+  if (form.invalid) {
+    form.control.markAllAsTouched();
+    return;
+  }
+
+  this.isLoading = true;
+
+  this.RegisterService.RegisterOwner(this.reg).subscribe({
+    next: () => {
+      this.isLoading = false;
+
+      this.toastr.success('Account created successfully!', 'Success', {
+        timeOut: 2500,
+        progressBar: true,
+        closeButton: true
+      });
+
+      this.router.navigate(['/owner/login']);
+    },
+    error: () => {
+      setTimeout(() => {
         this.isLoading = false;
-        
-        this.toastr.success('Account created successfully!', 'Success', {
-          timeOut: 2500,
+
+        this.toastr.error('Please check your details.', 'Registration Failed', {
+          timeOut: 4000,
           progressBar: true,
           closeButton: true
         });
-
-        this.router.navigate(['/owner/login']);
-      },
-      error: () => {
-        setTimeout(() => {
-          this.isLoading = false;
-          
-          this.toastr.error('Please check your details.', 'Registration Failed', {
-            timeOut: 4000,
-            progressBar: true,
-            closeButton: true
-          });
-        });
-      }
-    });
-  }
+      });
+    }
+  });
+}
 }
